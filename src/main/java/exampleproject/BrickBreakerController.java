b796022b63de7eec4ebd906a73babba4eb4fec87
Skjalg Skorstad <skjalgsk@stud.ntnu.no>
2022-03-02 08:53:39 +0100

    la til ny mappe: mastermind>App.fxml

---
+++ src/main/java/exampleproject/BrickBreakerController.java
@@ -0,0 +1,13 @@
+package exampleproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class BrickBreakerController {
+
+    @FXML
+    private Circle Ball;
+
+    @FXML
+
+}
---
+++ src/main/java/exampleproject/ball.java
@@ -0,0 +1,9 @@
+package exampleproject;
+
+public class hallo {
+
+    public static void main(String[] args) {
+        System.out.println("hall0");
+    }
+
+}
