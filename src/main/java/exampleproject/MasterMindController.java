31d277da8fd016948c13907700180901d0f2dc6e
johakn <johakn@stud.ntnu.no>
2022-03-16 17:53:11 +0100

    prøv å trykk på blue knappen

--- src/main/java/exampleproject/MasterMindController.java
+++ src/main/java/exampleproject/MasterMindController.java
@@ -7,12 +7,10 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 
 public class MasterMindController {
-    @FXML Button BLUE;
     @FXML Label label;
 
 //hvis man trykker på BLUE-knappen kommer det opp blue på første rute
     public void handleButtonClick(){
-        System.out.println("halla");
         label.setText("BLUE");
     }
     
