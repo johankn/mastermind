626d96e9b031cbc5c1df43c3b3bd04b8bc446fdf
johakn <johakn@stud.ntnu.no>
2022-03-16 16:59:22 +0100

    idk

--- src/main/java/exampleproject/MasterMindController.java
+++ src/main/java/exampleproject/MasterMindController.java
@@ -1,6 +1,13 @@
 package exampleproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
 public class MasterMindController {
-    
-    
+    @FXML
+    private void handleButtonClick() {
+    Circle circle = new Circle();
+      circle.setCenterX(300.0f);
+      circle.setCenterY(135.0f);
+      circle.setRadius(100.0f);
 }
